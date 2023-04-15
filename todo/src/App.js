815d7b0e4f930e75c6f4cc26ac109baf92bec856
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-04-15 18:36:03 +0300

    Update App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,93 +1,19 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
+import Main from "./listTast";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [filter, setFilter] = useState("all");
 
-  const Task=(props)=>{
-    const task = props.task;
-    return (
-      <li key={task.id}>
-        <div className="view">
-          <input className="toggle" defaultChecked={task.cheked} type="checkbox" />
-          <label>{task.text}</label>
-          <button className="destroy"></button>
-        </div>
-      </li>
-    );
-  }
+  return(
+  <div className="todoapp">
+    
+      <h1>todos</h1>
 
-  const addTask = (_text, _id = Date.now(), _checkbox = false) => {
-    if (_text) {
-      const newTask = { id: _id, text: _text, cheked: _checkbox };
-      setTasks([...tasks, newTask]);
-    }
-  };
-
-  const Filters = () => {
-    return (
-      <ul className="filters">
-        <li
-          onClick={() => {
-            setFilter("all");
-          }}
-        >
-          <a href="#/">All</a>
-        </li>
-        <li
-          onClick={() => {
-            setFilter("active");
-          }}
-        >
-          <a href="#/active">Active</a>
-        </li>
-        <li
-          onClick={() => {
-            setFilter("completed");
-          }}
-        >
-          <a href="#/completed">Completed</a>
-        </li>
-      </ul>
-    );
-  };
-
-  return (
-    <div className="App">
-      <body className="learn-bar">
-        <section className="todoapp">
-          <header className="header">
-            <h1>todos</h1>
-            <input
-              className="new-todo"
-              placeholder="What needs to be done?"
-              onBlur={(e) => {
-                addTask(e.target.value);
-                e.target.value = "";
-              }}
-            />
-          </header>
-          <section style={{ display: "block" }} className="main">
-            <input id="toggle-all" className="toggle-all" type="checkbox" />
-            <label htmlFor="toggle-all">Mark all as complete</label>
-            <ul className="todo-list">
-              {tasks.map((item) => (
-                <Task task={item}></Task>
-              ))}
-            </ul>
-            <footer className="footer">
-              <span className="todo-count">{tasks.length} item left</span>
-              <Filters/>
-              <button className="clear-completed" style={{ display: "none" }}>
-                Clear completed
-              </button>
-            </footer>
-            <div>{filter}</div>
-          </section>
-        </section>
-      </body>
-    </div>
+    <section style={{display: "block"}} className="main">
+   <Main></Main>
+   
+    </section>
+  </div>
   );
 }
 
