09b0cf32072f5ef5bab88dde2f9b7bbdae32f377
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-04-16 21:07:25 +0300

    Update App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,14 +3,24 @@ import React from "react";
 import Main from "./listTast";
 
 function App() {
+  const newTasksJson = localStorage.getItem("tasks");
+  let tasks = [];
+  if(newTasksJson)
+  {
+    tasks = JSON.parse(newTasksJson) ;
+  }
+  
 
   return(
+    
+      
+    
   <div className="todoapp">
     
       <h1>todos</h1>
 
     <section style={{display: "block"}} className="main">
-   <Main></Main>
+   <Main oldTasks={tasks}></Main>
    
     </section>
   </div>
