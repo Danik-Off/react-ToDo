b2c4262db00d870be6525c0ece07b897e1584e42
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-15 09:05:05 +0300

    Update footer.jsx

--- todo/src/components/footer.jsx
+++ todo/src/components/footer.jsx
@@ -1,37 +1,25 @@
 import Filter from "./filter";
-import React,{createContext,useContext} from "react";
-const FilterContext = createContext(null);
-export default function Footer({}) {
-    return(  <footer className="footer">
-    <span className="todo-count">
-      {/* {tasks.filter((task) => task.checked !== true).length} item left */}
-    </span>
-
-    <ul className="filters">
-      <Filter
-        text={"all"}
-        onClick={() => {
-          setFilter("all");
-        }}
-        active={filter === "all" ?? true}
-      />
-      <Filter
-        text={"active"}
-        onClick={() => {
-          setFilter("active");
-        }}
-        active={filter === "active" ?? true}
-      />
-      <Filter
-        text={"completed"}
-        onClick={() => {
-          setFilter("completed");
-        }}
-        active={filter === "completed" ?? true}
-      />
-    </ul>
-    <button className="clear-completed" style={{ display: "none" }}>
-      Clear completed
-    </button>
-  </footer>)
-};
+import React from "react";
+export default function Footer({ count, onChooseFilter, activeFilter }) {
+  const filters = ["all", "active", "completed"];
+  console.log("footer")
+  return (
+    <footer className="footer">
+      <span className="todo-count">{count} item left</span>
+      <ul className="filters"
+      onClick={(e)=>{
+        onChooseFilter(
+          e.target.innerHTML
+        );
+      }}
+      >
+        {filters.map(filter => (
+          <Filter text={filter} active={filter === activeFilter ?? true} />
+        ))}
+      </ul>
+      <button className="clear-completed" style={{ display: "none" }}>
+        Clear completed
+      </button>
+    </footer>
+  );
+}
